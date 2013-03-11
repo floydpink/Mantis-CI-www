1d649a5010cc0c0a9ea5ce2bc040d9a98e7fcc7d
Floyd Pink <imbleedingme@googlemail.com>
2013-03-11 17:49:23 -0400

    added an action for toggling description visibility
    Signed-off-by: Floyd Pink <imbleedingme@googlemail.com>

--- js/app/controllers/ReposController.js
+++ js/app/controllers/ReposController.js
@@ -1,5 +1,13 @@
 define([
   'ember'
 ], function (Ember) {
-  return Ember.ArrayController.extend();
+  return Ember.ArrayController.extend({
+
+    isInfoVisible: true,
+
+    toggleInfo: function () {
+      this.set('isInfoVisible', !this.get('isInfoVisible'));
+    }
+
+  });
 });
