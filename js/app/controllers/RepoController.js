95f4a2f5a3bcf5963ecfc4ffb3d4cee7b12b730c
Floyd Pink <imbleedingme@googlemail.com>
2013-03-27 15:48:44 -0400

    minor style change
    Signed-off-by: Floyd Pink <imbleedingme@googlemail.com>

--- js/app/controllers/RepoController.js
+++ js/app/controllers/RepoController.js
@@ -35,7 +35,7 @@ define([
     },
     previousTab            : function () {
       utils.debug('Previous');
-        Helpers.previousTab();
+      Helpers.previousTab();
     },
     nextTab                : function () {
       utils.debug('Next');
