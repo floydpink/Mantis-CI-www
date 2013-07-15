ff2169fb266ef689e322d4f553d48d5e938f284a
Floyd Pink <imbleedingme@googlemail.com>
2013-07-15 15:32:08 -0400

    show more is hidden for branch summary
    Signed-off-by: Floyd Pink <imbleedingme@googlemail.com>

--- js/app/controllers/BuildsController.js
+++ js/app/controllers/BuildsController.js
@@ -4,20 +4,23 @@ define([
   'app/utils'
 ], function (Ember, Build, utils) {
   return Ember.ArrayController.extend({
-    sortAscending    : false,
-    sortProperties   : ['number'],
-    needs            : ['repo'],
-    repoBinding      : 'controllers.repo.repo',
-    contentBinding   : 'controllers.repo.builds',
-    tabBinding       : 'controllers.repo.tab',
-    isLoadedBinding  : 'content.isLoaded',
-    isLoadingBinding : 'content.isLoading',
-    showMore         : function () {
+    sortAscending         : false,
+    sortProperties        : ['number'],
+    needs                 : ['repo'],
+    repoBinding           : 'controllers.repo.repo',
+    contentBinding        : 'controllers.repo.builds',
+    tabBinding            : 'controllers.repo.tab',
+    isLoadedBinding       : 'content.isLoaded',
+    isLoadingBinding      : 'content.isLoading',
+    showMore              : function () {
       utils.debug('BuildsRoute::showMore:>');
       var id, number;
       id = this.get('repo.id');
       number = this.get('lastObject.number');
       return this.get('content').load(Build.olderThanNumber(id, number, this.get('tab')));
-    }
+    },
+    displayShowMoreButton : function () {
+      return this.get('tab') !== 'branches';
+    }.property('tab')
   });
 });
