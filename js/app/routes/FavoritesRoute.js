0b26cc3c9f21887b60dfa447ec83942b5431dc79
Floyd Pink <imbleedingme@googlemail.com>
2013-07-17 18:17:25 -0400

    merged master
    Signed-off-by: Floyd Pink <imbleedingme@googlemail.com>

--- js/app/routes/FavoritesRoute.js
+++ js/app/routes/FavoritesRoute.js
@@ -1,14 +1,13 @@
 define([
-         'ember',
-         'ext/DontSetupModelForControllerMixin',
-         'models/Repo',
-         'ext/Favorites',
-         'app/utils'
-       ], function (Ember, DontSetupModelForControllerMixin, Repo, Favorites, utils) {
+  'ember',
+  'ext/DontSetupModelForControllerMixin',
+  'models/Repo',
+  'app/utils'
+], function (Ember, DontSetupModelForControllerMixin, Repo, utils) {
   return Ember.Route.extend(DontSetupModelForControllerMixin, {
     setupController : function () {
       utils.debug('FavoritesRoute::setupController:>');
-      this.container.lookup('controller:favorites').favorites(Favorites.getAll());
+      this.container.lookup('controller:favorites').activate();
     }
   });
 });
