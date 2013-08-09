d3f638659d9616200872a1e05e101e7153f7a726
Floyd Pink <imbleedingme@googlemail.com>
2013-08-09 03:02:49 -0400

    reinstating the splash images even for devices
    Signed-off-by: Floyd Pink <imbleedingme@googlemail.com>

--- js/app/routes/ReposRoute.js
+++ js/app/routes/ReposRoute.js
@@ -11,18 +11,15 @@ define([
 
   return Ember.Route.extend({
     afterModel : function () {
-      //show the splash only if this is the web-hosted version
-      if (!window.device) {
-        utils.debug('ReposRoute::afterModel:> seenSplash before reading cookie: ' + seenSplash);
-        seenSplash = seenSplash || $.cookie(seenSplashKey);
-        utils.debug('ReposRoute::afterModel:> seenSplash after reading cookie: ' + seenSplash);
+      utils.debug('ReposRoute::afterModel:> seenSplash before reading cookie: ' + seenSplash);
+      seenSplash = seenSplash || $.cookie(seenSplashKey);
+      utils.debug('ReposRoute::afterModel:> seenSplash after reading cookie: ' + seenSplash);
 
-        if (!seenSplash) {
-          seenSplash = true;
-          $.cookie(seenSplashKey, "true");
-          utils.debug('ReposRoute::afterModel:> Transition to splash');
-          this.transitionTo('splash');
-        }
+      if (!seenSplash) {
+        seenSplash = true;
+        $.cookie(seenSplashKey, "true");
+        utils.debug('ReposRoute::afterModel:> Transition to splash');
+        this.transitionTo('splash');
       }
     }
   });
