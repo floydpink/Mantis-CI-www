affdc7d21144aa8aa0117cb339e0911a9e99a452
Floyd Pink <imbleedingme@googlemail.com>
2013-08-09 16:31:26 -0400

    remove splash for just android [deploy]
    Signed-off-by: Floyd Pink <imbleedingme@googlemail.com>

--- js/app/routes/ReposRoute.js
+++ js/app/routes/ReposRoute.js
@@ -11,6 +11,10 @@ define([
 
   return Ember.Route.extend({
     afterModel : function () {
+      //do not show the splash on Android
+      if (window.device && window.device.platform.toLowerCase() === 'android') {
+        return;
+      }
       utils.debug('ReposRoute::afterModel:> seenSplash before reading cookie: ' + seenSplash);
       seenSplash = seenSplash || $.cookie(seenSplashKey);
       utils.debug('ReposRoute::afterModel:> seenSplash after reading cookie: ' + seenSplash);
