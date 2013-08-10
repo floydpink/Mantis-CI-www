e260c2374dceb03f0f6ebb15b703510657175719
Floyd Pink <imbleedingme@googlemail.com>
2013-08-09 20:19:47 -0400

    use cordova splashscreen for devices [deploy]
    Signed-off-by: Floyd Pink <imbleedingme@googlemail.com>

--- js/app/routes/ReposRoute.js
+++ js/app/routes/ReposRoute.js
@@ -11,19 +11,22 @@ define([
 
   return Ember.Route.extend({
     afterModel : function () {
-      //do not show the splash on Android
-      if (window.device && window.device.platform.toLowerCase() === 'android') {
-        return;
-      }
-      utils.debug('ReposRoute::afterModel:> seenSplash before reading cookie: ' + seenSplash);
-      seenSplash = seenSplash || $.cookie(seenSplashKey);
-      utils.debug('ReposRoute::afterModel:> seenSplash after reading cookie: ' + seenSplash);
+      //show the splash only if this is the web-hosted version
+      if (!window.device) {
+        utils.debug('ReposRoute::afterModel:> seenSplash before reading cookie: ' + seenSplash);
+        seenSplash = seenSplash || $.cookie(seenSplashKey);
+        utils.debug('ReposRoute::afterModel:> seenSplash after reading cookie: ' + seenSplash);
 
-      if (!seenSplash) {
-        seenSplash = true;
-        $.cookie(seenSplashKey, "true");
-        utils.debug('ReposRoute::afterModel:> Transition to splash');
-        this.transitionTo('splash');
+        if (!seenSplash) {
+          seenSplash = true;
+          $.cookie(seenSplashKey, "true");
+          utils.debug('ReposRoute::afterModel:> Transition to splash');
+          this.transitionTo('splash');
+        }
+      } else {
+        if (navigator && navigator.splashscreen && navigator.splashscreen.hide) {
+          navigator.splashscreen.hide();
+        }
       }
     }
   });
